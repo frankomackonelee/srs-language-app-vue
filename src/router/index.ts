f57c8d127ba58ed4abc4470c08dd8488872d22f8
Francis Malone-Lee <francis@mathskitchen.com>
2024-01-20 13:05:37 +0000

    Redirect to home client side when no match for route

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,6 +22,12 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/ProfileView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*', 
+      // This will redirect any unmatched route to home.  This might cause some confusion in the future...
+      // TODO: add a NOT FOUND view
+      redirect: '/',
     }
   ]
 })
